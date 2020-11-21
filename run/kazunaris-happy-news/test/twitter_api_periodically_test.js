853a1ef8afd5a05f92510c7ab7342c6e7c4cd70f
shinagawa_joy <shinagawa@joyworks.jp>
2020-11-21 17:44:24 +0900

    CSV出力npm追加。教師データ作成用CSV出力JSを追加。※CSVはローカルで管理中、コミットはしない予定。

--- run/kazunaris-happy-news/test/twitter_api_periodically_test.js
+++ run/kazunaris-happy-news/test/twitter_api_periodically_test.js
@@ -1,13 +1,13 @@
-let Twitter = require('twitter');
+const Twitter = require('twitter');
 
-let client = new Twitter({
+const client = new Twitter({
   consumer_key:        'bMuXNzI4awteVBneZWDczSvgK',
   consumer_secret:     'iq79SpnbnWzxI2an8s3mVdCG7690rhaY7UMMLZtxtmNbNt6k72',
   access_token_key:    '1319826967416369152-b4SbQEObe23UqM5CC9NZtnXT8TttTA',
   access_token_secret: 'ZZ7PcGXhMwhkwDv4BPmnjw3yEdRyGf2SCyqCFLGPWFwvf',
 });
 
-let cron = require('node-cron');
+const cron = require('node-cron');
  
 cron.schedule('* * * * *', () => {
   console.log(tweetsget());
